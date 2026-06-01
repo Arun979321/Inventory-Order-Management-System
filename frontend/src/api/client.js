4a0f35809947819ba2c353882f1ef4e22bc38e8a
ARUN KUMAR <148804375+Arun979321@users.noreply.github.com>
2026-06-01 18:52:25 +0530

    Update client.js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+const API_URL = process.env.REACT_APP_API_URL || 'https://inventory-order-management-system-wxl9.onrender.com/';
 
 const client = axios.create({
   baseURL: API_URL,
